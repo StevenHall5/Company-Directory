18d5667ffb9fffbcd3a5bd110ce424416fcf063d
StevenHall5 <73425435+StevenHall5@users.noreply.github.com>
2021-01-22 14:38:52 +0000

    Setting up the basic framework of my system

---
+++ libs/js/myscript.js
@@ -0,0 +1,19 @@
+$('#button1').click(function() {
+	$('#display').html('You pressed 1');
+});
+
+$('#button2').click(function() {
+	$('#display').html('You pressed 2');
+});
+
+$('#button3').click(function() {
+	$('#display').html('You pressed 3');
+});
+
+$('#button4').click(function() {
+	$('#display').html('You pressed 4');
+});
+
+$('#button5').click(function() {
+	$('#display').html('You pressed 5');
+});
